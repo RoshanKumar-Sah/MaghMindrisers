b64390df8b977e8767df8cd76374b1f18a1b99f1
Roshan Sah <mail4sahrs@gmail.com>
2023-03-03 09:16:20 +0545

    javascipt folder added

---
+++ Javascript/areaofrectangle.js
@@ -0,0 +1,5 @@
+let length = 12 //declaration and initialization of variable
+let breadth = 20 //declaration and initialization of variable
+let prefix = "The area of rectangle having length " //declaration and initialization of variable
+let area = (length * breadth) //declaration, initialization of variable and performing calculation
+console.log(prefix + length + " and breadth " + breadth + " is " + area) // printing output
